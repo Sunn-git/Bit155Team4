e7e23f2f1ff3e8677c1220c4fdb85d6aff144777
test_git <geranium236@gmail.com>
2020-03-31 20:06:22 +0900

    C5-5

--- Team4_Git_Collaboration/src/Intro.java
+++ Team4_Git_Collaboration/src/Intro.java
@@ -3,7 +3,7 @@ public class Intro {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요. 4조 git 콜라보 프로젝트 입니다.");
-		System.out.println("C5");
+		System.out.println("C5-5");
 	}
 
 }
